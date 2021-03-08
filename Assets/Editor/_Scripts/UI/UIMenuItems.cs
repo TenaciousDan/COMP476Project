2999a762054ad279ade4dc7df9d6ac40c882a60a
TenaciousDan <danieldavidrinaldi@gmail.com>
2021-03-07 21:30:02 -0500

    UI mostly complete
    - UI components have been separated into prefab for reuse
    - might have to do some touch ups to some pixels in a few sprites but I'll do that later on (my eyes are tired of looking at pixels)
    - GameObject Context Menu Items added for easily creating UI

--- Assets/Editor/_Scripts/UI/UIMenuItems.cs
+++ Assets/Editor/_Scripts/UI/UIMenuItems.cs
@@ -19,6 +19,12 @@ namespace Editor.UI
         public static readonly string TEXT_PREFAB_PATH = AssetPaths.PREFAB_DIR + "UI/Text.prefab";
         public static readonly string DROPDOWN_PREFAB_PATH = AssetPaths.PREFAB_DIR + "UI/Dropdown.prefab";
         public static readonly string HEADER_PREFAB_PATH = AssetPaths.PREFAB_DIR + "UI/Header.prefab";
+        public static readonly string INPUTFIELD_PREFAB_PATH = AssetPaths.PREFAB_DIR + "UI/InputField.prefab";
+        public static readonly string SLIDER_PREFAB_PATH = AssetPaths.PREFAB_DIR + "UI/Slider.prefab";
+        public static readonly string PROGRESSBAR_PREFAB_PATH = AssetPaths.PREFAB_DIR + "UI/ProgressBar.prefab";
+        public static readonly string PROGRESSKNOB_OUTLINE_PREFAB_PATH = AssetPaths.PREFAB_DIR + "UI/ProgressKnob (Outline).prefab";
+        public static readonly string PROGRESSKNOB_PREFAB_PATH = AssetPaths.PREFAB_DIR + "UI/ProgressKnob.prefab";
+        public static readonly string PROGRESSPIE_PREFAB_PATH = AssetPaths.PREFAB_DIR + "UI/ProgressPie.prefab";
 
         public static int[] textSizes = new int[6] { 256, 192, 128, 64, 42, 28 };
 
@@ -88,6 +94,42 @@ namespace Editor.UI
                 RectTransform rectTransform = (RectTransform)g.transform;
                 rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, fontSize);
             }
+        }
+
+        [MenuItem(MenuItems.GAMEOBJECT_PREFAB_UI_MENUITEM_PATH + "InputField")]
+        static void InputFieldPrefabMenuItem()
+        {
+            EditorUtil.CreatePrefab("InputField", INPUTFIELD_PREFAB_PATH);
+        }
+
+        [MenuItem(MenuItems.GAMEOBJECT_PREFAB_UI_MENUITEM_PATH + "Slider")]
+        static void SliderPrefabMenuItem()
+        {
+            EditorUtil.CreatePrefab("Slider", SLIDER_PREFAB_PATH);
+        }
+
+        [MenuItem(MenuItems.GAMEOBJECT_PREFAB_UI_MENUITEM_PATH + "Progress/ProgressBar")]
+        static void ProgressBarPrefabMenuItem()
+        {
+            EditorUtil.CreatePrefab("ProgressBar", PROGRESSBAR_PREFAB_PATH);
+        }
+
+        [MenuItem(MenuItems.GAMEOBJECT_PREFAB_UI_MENUITEM_PATH + "Progress/ProgressKnob (Outline)")]
+        static void ProgressKnobOutlinePrefabMenuItem()
+        {
+            EditorUtil.CreatePrefab("ProgressKnob (Outline)", PROGRESSKNOB_OUTLINE_PREFAB_PATH);
+        }
+
+        [MenuItem(MenuItems.GAMEOBJECT_PREFAB_UI_MENUITEM_PATH + "Progress/ProgressKnob")]
+        static void ProgressKnobPrefabMenuItem()
+        {
+            EditorUtil.CreatePrefab("ProgressKnob", PROGRESSKNOB_PREFAB_PATH);
+        }
+
+        [MenuItem(MenuItems.GAMEOBJECT_PREFAB_UI_MENUITEM_PATH + "Progress/ProgressPie")]
+        static void ProgressPiePrefabMenuItem()
+        {
+            EditorUtil.CreatePrefab("ProgressPie", PROGRESSPIE_PREFAB_PATH);
         }
     }
 }
