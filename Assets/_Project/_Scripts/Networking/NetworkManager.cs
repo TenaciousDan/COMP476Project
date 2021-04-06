e65e117e62bfc86912a75be7297c8cf8e141ec64
Weiliang Xie <weiliangxie@hotmail.com>
2021-04-06 19:08:02 -0400

    Added more information for singleton Network Manager

--- Assets/_Project/_Scripts/Networking/NetworkManager.cs
+++ Assets/_Project/_Scripts/Networking/NetworkManager.cs
@@ -1,24 +1,69 @@
 using Photon.Pun;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
+    private static NetworkManager instance;
+    private static bool reinitializationPermitted = false;
+    private static bool destroyed = false;
+    private static object mutex = new object();
+
+    [Tooltip("Allow this object to be re-initialized after being destroyed.")]
+    [SerializeField] private bool allowReinitialization = false;
+    [Tooltip("Destroy this object when a new scene is loaded.")]
+    [SerializeField] private bool destroyOnLoad = false;
+
     // Instance
-    public static NetworkManager Instance;
+    public static NetworkManager Instance
+    {
+        get
+        {
+            if (destroyed)
+            {
+                string log_message = "NetworkManager has already been destroyed";
+
+                if (reinitializationPermitted)
+                {
+                    Debug.LogWarning(log_message + " - Creating new instance.");
+                    destroyed = false;
+                }
+                else
+                {
+                    Debug.LogWarning(log_message + " - Returning null.");
+                    return null;
+                }
+            }
+
+            return CreateInstance(null);
+        }
+    }
 
     private void Awake()
     {
-        if (Instance != null && Instance != this)
+        if(instance == null)
         {
-            gameObject.SetActive(false);
+            if (destroyed && !allowReinitialization)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+            else
+                CreateInstance(this.gameObject);
         }
         else
         {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
+            // an instance already exists and this is a duplicate
+            Destroy(this.gameObject);
+            destroyed = false; // the "true" instance has not yet been destroyed
+            return;
         }
+
+        reinitializationPermitted = allowReinitialization;
+
+        if (!DestroyOnLoad)
+            DontDestroyOnLoad(this.gameObject);
     }
 
     public override void OnConnectedToMaster()
@@ -26,6 +71,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         print("Connected to master server!");
     }
 
+    #region ROOMS
     public void CreateRoom(string roomName)
     {
         PhotonNetwork.CreateRoom(roomName);
@@ -45,10 +91,60 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     {
         print(message);
     }
+    #endregion
 
     [PunRPC]
     public void ChangeScene(string sceneName)
     {
         PhotonNetwork.LoadLevel(sceneName);
     }
+
+    #region SINGLETON
+    private static NetworkManager CreateInstance(GameObject game_object)
+    {
+        lock (mutex)
+        {
+            // Create new instance if one doesn't already exist
+            if (instance == null)
+            {
+                if (game_object == null)
+                {
+                    // create the GameObject that will house the singleton instance
+                    game_object = new GameObject("NetworkManager");
+                    game_object.AddComponent<NetworkManager>();
+                }
+
+                instance = game_object.GetComponent<NetworkManager>();
+            }
+
+            return instance;
+        }
+    }
+    public bool DestroyOnLoad
+    {
+        get { return destroyOnLoad; }
+        set
+        {
+            destroyOnLoad = value;
+            if (destroyOnLoad)
+                SceneManager.MoveGameObjectToScene(this.gameObject, SceneManager.GetActiveScene());
+            else
+                DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        // When Unity quits it destroys objects in a random order and this can create issues for singletons. 
+        // So we prevent reinitialization and access to the instance when the application quits to prevent problems.
+        reinitializationPermitted = false;
+        destroyed = true; // pretend its already destroyed
+    }
+
+    private void OnDestroy()
+    {
+        instance = null;
+        destroyed = true;
+    }
+    #endregion
 }
