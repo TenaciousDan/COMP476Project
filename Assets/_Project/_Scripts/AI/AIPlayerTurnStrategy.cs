15781e424136325e20302e9f7babd567cbacf46e
TenaciousDan <danieldavidrinaldi@gmail.com>
2021-04-08 18:29:37 -0400

    added interface for implementing AIPlayerTurnStrategy

---
+++ Assets/_Project/_Scripts/AI/AIPlayerTurnStrategy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Game.AI
+{
+    public class AIPlayerTurnStrategy : MonoBehaviour, IPlayerTurnStrategy
+    {
+        [SerializeField] private PlayerController player;
+        [SerializeField] private BehaviorTree behaviorTree;
+
+        public void doMainPhase()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void computePath()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void useItem()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int IsItemBoxAvailable()
+        {
+            return (int)BTNode.EState.Success;
+        }
+    }
+}
---
+++ Assets/_Project/_Scripts/Player/IPlayerTurnStrategy.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public interface IPlayerTurnStrategy
+{
+    public void doMainPhase();
+    public void computePath();
+    public void useItem();
+}
