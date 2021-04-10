c38ed7abcb8b30630189e291bed300c572a2b31a
Weiliang Xie <weiliangxie@hotmail.com>
2021-04-10 19:01:28 -0400

    Added mock player ui script and test scene, missing other player info

---
+++ Assets/_Project/Sandbox/PlayerUITest/PlayerUI.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerUI : MonoBehaviour
+{
+    public void MoveBtnClick()
+    {
+        print("Time to move!");
+        // TODO - Call move for player
+    }
+
+    public void FirstItemBtnClick()
+    {
+        print("Using item1");
+        // TODO - Use item1
+    }
+
+    public void SecondItemBtnClick()
+    {
+        print("Using item2");
+        // TODO - Use item2
+    }
+
+    public void ThirdItemBtnClick()
+    {
+        print("Using item3");
+        // TODO - Use item3
+    }
+
+    public void EndTurnBtnClick()
+    {
+        print("Ending turn!");
+        // TODO - End current turn.
+    }
+}
