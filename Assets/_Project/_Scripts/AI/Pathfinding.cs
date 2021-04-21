c611b7ef95d196f82ac9fd026fd52ee8a94ecdfa
TenaciousDan <danieldavidrinaldi@gmail.com>
2021-04-21 10:14:48 -0400

    OilSpill now affects cost in pathfinding

--- Assets/_Project/_Scripts/AI/Pathfinding.cs
+++ Assets/_Project/_Scripts/AI/Pathfinding.cs
@@ -72,6 +72,9 @@ namespace Game.AI
                     if (closedODict.Contains(n.Id))
                         continue;
 
+                    if (graph[n.Id].Data.GetComponentInChildren<OilSpillManager>() != null)
+                        cost += 2;
+
                     float gNeighbor = gnDict[currentId] + cost;
                     if (!gnDict.ContainsKey(n.Id) || gNeighbor < gnDict[n.Id])
                     {
