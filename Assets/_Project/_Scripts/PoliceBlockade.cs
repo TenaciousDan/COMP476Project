e3d20c68400cebb028591c0f63e6e1accdb6fa4a
TenaciousDan <danieldavidrinaldi@gmail.com>
2021-04-24 12:56:34 -0400

    PoliceBlockade script

--- Assets/_Project/_Scripts/PoliceBlockade.cs
+++ Assets/_Project/_Scripts/PoliceBlockade.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PoliceBlockade : MonoBehaviour
+using Photon.Pun;
+
+using System.Collections.Generic;
+
+public class PoliceBlockade : MonoBehaviourPun
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<Animator> copCarAnimators = new List<Animator>();
+
+    private void OnTriggerStay(Collider other)
     {
-        
+        AbstractPlayer player = other.GetComponent<AbstractPlayer>();
+        if (player != null)
+        {
+            if (GameplayManager.Instance.Players[GameplayManager.Instance.currentPlayer] == player)
+            {
+                photonView.RPC("LetMePass", RpcTarget.All, true);
+            }
+            else
+            {
+                photonView.RPC("LetMePass", RpcTarget.All, false);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        photonView.RPC("LetMePass", RpcTarget.All, false);
+    }
+
+    [PunRPC]
+    private void LetMePass(bool pass)
+    {
+        foreach (Animator animator in copCarAnimators)
+        {
+            animator.SetBool("CanLeave", pass);
+        }
     }
 }
